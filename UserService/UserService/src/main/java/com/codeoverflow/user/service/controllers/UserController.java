480ffef713ed12111d3c137dbe69f94d15a64d8a
vijayypandit <panditvijay551@gmai.com>
2026-04-08 10:34:50 +0530

    Implemented Circuit breaker using resillance4j and Retry calls to User-Rating Service

--- UserService/UserService/src/main/java/com/codeoverflow/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/codeoverflow/user/service/controllers/UserController.java
@@ -1,42 +1,73 @@
 package com.codeoverflow.user.service.controllers;
 
+import java.util.List;
 
 import com.codeoverflow.user.service.entities.User;
 import com.codeoverflow.user.service.services.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @Autowired
-    private UserService userService;
-    //create
+  @Autowired
+  private UserService userService;
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user)
-    {
-          User user1 =   userService.saveUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user1);
-    }
+  private Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    //get single user
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getSingleUser(@PathVariable String userId){
-      User user =  userService.getUser(userId);
-      return ResponseEntity.ok(user);
-    }
+  @PostMapping
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+    User user1 = userService.saveUser(user);
+    return ResponseEntity.status(HttpStatus.CREATED).body(user1);
+  }
 
-    //get all users
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUsers(){
+  int retryCount =1;
+  // get single user
+  @GetMapping("/{userId}")
+  // @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+  @Retry(name="ratingHotelServce" , fallbackMethod = "ratingHotelFallback")
+  public ResponseEntity<User> getSingleUser(@PathVariable String userId) 
+  {
+    logger.info("Retry count : {} " , retryCount);
+    retryCount++;
+    User user = userService.getUser(userId);
+    return ResponseEntity.ok(user);
+  }
 
-          List<User> users=  userService.getAllUser();
-          return ResponseEntity.ok(users);
-    }
+
+  // creating fallback method for circuit breaker ...
+  public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+    
+    // logger.info("Fallback is executed because service is down : ex.getMessage()");
+    
+    User user = User.builder()
+        .email("dummy@gmail.com")
+        .name("Dummy")
+        .about("This user is created because the service is down .")
+        .userId("12125")
+        .build();
+
+    return new ResponseEntity<>(user, HttpStatus.OK);
+  }
+
+  // get all users
+  @GetMapping
+  public ResponseEntity<List<User>> getAllUsers() {
+
+    List<User> users = userService.getAllUser();
+    return ResponseEntity.ok(users);
+  }
 
 }
--- UserService/UserService/src/main/java/com/codeoverflow/user/service/entities/User.java
+++ UserService/UserService/src/main/java/com/codeoverflow/user/service/entities/User.java
@@ -1,25 +1,31 @@
 package com.codeoverflow.user.service.entities;
 
-import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
 @Entity
-@Table(name="micro_users")
+@Table(name = "users")
 public class User {
     @Id
-    @Column(name="ID")
+    @Column(name = "ID")
     private String userId;
-    @Column(name="NAME", length = 30)
+    @Column(name = "NAME", length = 30)
     private String name;
-    @Column(name="EMAIL")
+    @Column(name = "EMAIL")
     private String email;
-    @Column(name="ABOUT")
+    @Column(name = "ABOUT")
     private String about;
     @Transient
     private List<Rating> ratings = new ArrayList<>();
 
-    public User() {}
+    public User() {
+    }
 
     public User(String userId, String name, String email, String about, List<Rating> ratings) {
         this.userId = userId;
@@ -29,25 +35,84 @@ public class User {
         this.ratings = ratings;
     }
 
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getAbout() { return about; }
-    public void setAbout(String about) { this.about = about; }
-    public List<Rating> getRatings() { return ratings; }
-    public void setRatings(List<Rating> ratings) { this.ratings = ratings; }
+    public String getUserId() {
+        return userId;
+    }
 
-    public static UserBuilder builder() { return new UserBuilder(); }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
     public static class UserBuilder {
-        private String userId; private String name; private String email; private String about; private List<Rating> ratings = new ArrayList<>();
-        public UserBuilder userId(String userId) { this.userId = userId; return this; }
-        public UserBuilder name(String name) { this.name = name; return this; }
-        public UserBuilder email(String email) { this.email = email; return this; }
-        public UserBuilder about(String about) { this.about = about; return this; }
-        public UserBuilder ratings(List<Rating> ratings) { this.ratings = ratings; return this; }
-        public User build() { return new User(userId, name, email, about, ratings); }
+        private String userId;
+        private String name;
+        private String email;
+        private String about;
+        private List<Rating> ratings = new ArrayList<>();
+
+        public UserBuilder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder about(String about) {
+            this.about = about;
+            return this;
+        }
+
+        public UserBuilder ratings(List<Rating> ratings) {
+            this.ratings = ratings;
+            return this;
+        }
+
+        public User build() {
+            return new User(userId, name, email, about, ratings);
+        }
     }
 }
